54f778760040b6d8b26161f54dfca064ccbee344
Abigail McPhillips <abigail.mcphillips@gmail.com>
2020-11-12 15:50:26 +0000

    Add new article and video

--- website/config-constants/articles.js
+++ website/config-constants/articles.js
@@ -1,36 +1,38 @@
 function articles(baseUrl) {
   return [
     {
-      content:
-        "Send Cavy test results to iOS or Android test runners, or write your own report handler.",
-      title: "Cavy — better test reporting for React Native integration tests",
-      author: 'Abigail McPhillips',
+      content: "Run specific tests, receive tests results as they are run and configure Cavy to timeout if your app fails to build.",
+      title: "New Features for Cavy 4.0 🐹",
+      author: 'Jonathan Andrews',
       image: baseUrl + "img/cavy.png",
-      link:
-        "https://blog.pixielabs.io/cavy-better-test-reporting-for-react-native-integration-tests-49cab1efa60b",
+      link: "https://blog.pixielabs.io/new-features-for-cavy-4-0-98a203f35f2e",
+      date: "Jun 12, 2020",
+      type: "Blog"
+    },
+    {
+      content: "Send Cavy test results to iOS or Android test runners, or write your own report handler.",
+      title: "Cavy — better test reporting",
+      author: 'Abigail McPhillips',
+      image: baseUrl + "img/heart-eyes-cavy.png",
+      link: "https://blog.pixielabs.io/cavy-better-test-reporting-for-react-native-integration-tests-49cab1efa60b",
       date: "Jun 4, 2019",
       type: "Blog"
     },
     {
-      content:
-        "Introducing the useCavy() React Hook for function components and the beforeEach test function.",
+      content: "Introducing the useCavy() React Hook for function components and the beforeEach test function.",
       title: "Cavy 2.0 - Shiny new features",
       author: 'Abigail McPhillips',
       image: baseUrl + "img/cavy.png",
-      link:
-        "https://blog.pixielabs.io/cavy-2-0-shiny-new-features-f0b5449b23dd",
+      link: "https://blog.pixielabs.io/cavy-2-0-shiny-new-features-f0b5449b23dd",
       date: "May 8, 2019",
       type: "Blog"
     },
     {
-      content:
-        "In this post, I’ll go over some first impressions and a few patterns I’ve found to be helpful.",
-      title: "An Introduction to React Native Testing with Cavy",
+      content: "In this post, I’ll go over some first impressions and a few patterns I’ve found to be helpful.",
+      title: "Intro to RN Testing with Cavy",
       author: 'Tyler Hoffman',
-      image:
-        "https://pbs.twimg.com/profile_images/1125887739860275200/wytFQDmu_400x400.png",
-      link:
-        "https://spin.atomicobject.com/2019/02/11/react-native-testing-cavy/",
+      image: "https://pbs.twimg.com/profile_images/1125887739860275200/wytFQDmu_400x400.png",
+      link: "https://spin.atomicobject.com/2019/02/11/react-native-testing-cavy/",
       date: "Feb 11, 2019",
       type: "Article"
     },
@@ -43,24 +45,28 @@ function articles(baseUrl) {
       type: "Video"
     },
     {
-      content:
-        "How we at Magnetis Backstage are writing e2e tests for our mobile application.",
+      content: "How we at Magnetis Backstage are writing e2e tests for our mobile application.",
       title: "React Native e2e testing with Cavy",
       author: 'Luiz Parreira',
-      image: baseUrl + "img/cavy.png",
-      link:
-        "https://medium.com/magnetis-backstage/react-native-e2e-testing-with-cavy-1f1d5be1d3be",
+      image: baseUrl + "img/heart-eyes-cavy.png",
+      link: "https://medium.com/magnetis-backstage/react-native-e2e-testing-with-cavy-1f1d5be1d3be",
       date: "Mar 28, 2018",
       type: "Blog"
     },
     {
-      content:
-        "Cavy is an open-source end-to-end integration test framework for React Native, developed for use with both iOS and Android applications. ",
+      title: "Cavy: Testing React Native apps",
+      author: 'Abigail McPhillips & David Somers',
+      image: baseUrl + "img/Facebook.png",
+      link: "https://www.youtube.com/watch?v=p3smdLQauW8&t",
+      date: "May 25, 2017",
+      type: "Video"
+    },
+    {
+      content: "Cavy is an open-source end-to-end integration test framework for React Native, developed for use with both iOS and Android applications. ",
       title: "Announcing Cavy",
       author: 'Abigail McPhillips',
       image: baseUrl + "img/cavy.png",
-      link:
-        "https://blog.pixielabs.io/announcing-cavy-an-integration-testing-framework-for-react-native-b09d264923d9",
+      link: "https://blog.pixielabs.io/announcing-cavy-an-integration-testing-framework-for-react-native-b09d264923d9",
       date: "Oct 31, 2017",
       type: "Blog"
     }
